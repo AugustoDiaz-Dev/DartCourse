a38be0afdd0fc33d9c89b3d6f8d89f1bda66f67a
AUGUSTO  DIAZ <augustodiaz@AUGUSTOs-MacBook-Pro.local>
2023-06-19 11:52:20 +1000

    Dart Course

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# DartCourse
+# D A R T
+
+- // To run your code use: dart playground.dart
+
+- // Example 1 - main function
+
+- // Example 2 - Types basics
+
+- // Example 3 - Import a package and get data form the user
+
+- // Example 4 - Data types
---
+++ playground.dart
@@ -0,0 +1,28 @@
+// D A R T
+// To run your code use: dart playground.dart
+
+// Example 1 - main function
+// void main() {
+//   print('Hello Augusto!');
+// }
+
+// Example 2 - Types basics
+// void main() {
+//   // Here Dart uses type inference:
+//   var myName = "Augusto";
+//   // If you declare the type String you only can use strings:
+//   String myLastName = "Diaz";
+//   print(" $myName $myLastName!");
+// }
+
+// Example 3 - Import a package and get data form the user
+// import 'dart:io';
+
+// void main() {
+//   stdout.writeln('What is your name?');
+//   String name = stdin.readLineSync()!;
+//   print('My name is $name.');
+// }
+
+// Example 4 - Data types
+void main() {}
