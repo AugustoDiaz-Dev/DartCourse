6a5b4fbbd6000ff2dedb8a3c1f5a8320199b156e
AUGUSTO  DIAZ <augustodiaz@AUGUSTOs-MacBook-Pro.local>
2023-06-21 23:48:42 +1000

    Dart Course

--- playground.dart
+++ playground.dart
@@ -1,9 +1,21 @@
 // D A R T
 // To run your code use: dart playground.dart
 
-// Example 1 - main function
+// In Strongly Typed Language: The type of a variaable is known at compile time. For example: C++, Java, Swift, etc.
+
+// In Dynammic Typed Language: The type of a variable is know at run time. For example: Pythonk Ruby, JavaScript.
+
+// Example 4 - Data types
+// In Dart there are 5 basic data types: int, double, String, bool, dynamic.
+void main() {}
+
+// Example 3 - Import a package and get data form the user
+// import 'dart:io';
+
 // void main() {
-//   print('Hello Augusto!');
+//   stdout.writeln('What is your name?');
+//   String name = stdin.readLineSync()!;
+//   print('My name is $name.');
 // }
 
 // Example 2 - Types basics
@@ -15,14 +27,7 @@
 //   print(" $myName $myLastName!");
 // }
 
-// Example 3 - Import a package and get data form the user
-// import 'dart:io';
-
+// Example 1 - main function
 // void main() {
-//   stdout.writeln('What is your name?');
-//   String name = stdin.readLineSync()!;
-//   print('My name is $name.');
+//   print('Hello Augusto!');
 // }
-
-// Example 4 - Data types
-void main() {}
